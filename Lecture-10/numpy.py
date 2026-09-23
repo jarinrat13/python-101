b37700903d15f44efb50a03713d9d9d5d8442417
jarinrat13 <jarinrat042@gmail.com>
2026-09-23 09:34:30 +0700

    Add files via upload

---
+++ Lecture-10/numpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+random_matrix = np.random.randint(1, 11, size=(3, 3))
+print("Random 3x3 Matrix:\n", random_matrix)
+
+matrix_sum = np.sum(random_matrix)
+print(f"\nSum od all elements: {matrix_sum}")
+
+transposed_matrix = np.transpose(random_matrix)
+print("\nTransposed Martix:\n", transposed_matrix)
